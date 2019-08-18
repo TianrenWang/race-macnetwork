9041a1f9243005afc4b4dc139cc0f9c1682beb9b
TianrenWang <broodstar_frank@hotmail.com>
2019-08-17 21:31:03 -0400

    first commit

---
+++ container/RACE/MACRNN.py
@@ -0,0 +1,137 @@
+import tensorflow as tf
+from transformer import Transformer
+
+# num_layers = 2
+# d_model = 512
+# dff = 512
+# num_heads = 8
+# dropout_rate = 0.1
+
+class Controller(tf.keras.layers.Layer):
+    def __init__(self, d_model): #, num_layers, num_heads, dff, rate):
+        super(Controller, self).__init__()
+
+        self.cq = tf.keras.layers.Dense(d_model)
+        self.focus = tf.keras.layers.Dense(1)
+        # self.transformer = Transformer(num_layers, d_model, num_heads, dff, rate)
+
+
+    def call(self, quest_state, control, context, training):
+        # quest_state (the state of the question in each reasoning step): [batch, d]
+        # control (the operation to be done this step): [batch, d]
+        # context (BERT-encoded question): [batch, seq_len, d]
+
+        cq = self.cq(tf.concat([quest_state, control])) #[batch, d]
+        focus = self.focus(cq * context) # [batch, seq_len, d]
+        focus = tf.keras.backend.squeeze(focus, 2)
+
+        #attention, attention_weights = self.transformer(context, focus, training) #attention: [batch, seq_len, d]
+
+        attention = tf.nn.softmax(focus) #[batch, seq_len]
+
+        return tf.reduce_mean(attention * context, 1), attention # [batch, d]
+
+
+class Reader(tf.keras.layers.Layer):
+    def __init__(self, d_model): #, num_layers,  num_heads, dff, rate):
+        super(Reader, self).__init__()
+
+        self.memory = tf.keras.layers.Dense(d_model)
+        self.knowledge = tf.keras.layers.Dense(d_model)
+        self.disjoint = tf.keras.layers.Dense(d_model)
+        self.retrieve = tf.keras.layers.Dense(1)
+        #self.transformer = Transformer(num_layers, d_model, num_heads, dff, rate)
+
+    def call(self, memory, knowledge, control, training):
+        # memory (the memory of the previous reasoning step): [d]
+        # control (the operation to be done this step): [d]
+        # context (BERT-encoded question): [seq_len, d]
+
+        reflection = self.memory(memory) * self.knowledge(knowledge)
+        disjoint = self.disjoint(tf.concat([knowledge, reflection], 2))
+        retrieve = self.retrieve(disjoint * control)
+        retrieve = tf.keras.backend.squeeze(retrieve, 2)
+
+        attention = tf.nn.softmax(retrieve) #[batch, seq_len]
+
+        #attention, attention_weights = self.transformer(knowledge, retrieve, training)
+        return tf.reduce_mean(attention * knowledge, 1), attention # [batch, d]
+
+
+class Writer(tf.keras.layers.Layer):
+    def __init__(self, d_model):
+        super(Reader, self).__init__()
+
+        self.m1 = tf.keras.layers.Dense(d_model)
+        self.control_attention = tf.keras.layers.Dense(1)
+        self.retrieve = tf.keras.layers.Dense(d_model)
+        self.m2 = tf.keras.layers.Dense(d_model)
+        self.s = tf.keras.layers.Dense(d_model)
+        self.m3 = tf.keras.layers.Dense(1)
+
+    def call(self, memory, read, control, past_results, training):
+        # past_results (concatenation of all previous controls and memories): [batch, iterations, 2d]
+
+        past_controls, past_memories = tf.split(past_results, 2, axis=2)
+
+        m1 = self.m1(tf.concat([memory, read], 2))
+
+        control_attention = self.control_attention(past_controls * control)
+        control_softmax = tf.keras.activations.softmax(control_attention)
+        msa = tf.reduce_sum(control_softmax * past_memories, 1)
+
+        mp = self.m2(m1) + self.s(msa)
+
+        control_gate = tf.nn.sigmoid(self.m3(control))
+
+        new_memory = mp * control_gate + (1 - control_gate) * memory
+
+        return new_memory # [batch, d]
+
+class Output(tf.keras.layers.Layer):
+    def __init__(self, d_model, classes):
+        super(Output, self).__init__()
+
+        self.dense1 = tf.keras.layers.Dense(d_model)
+        self.dense2 = tf.keras.layers.Dense(classes)
+
+    def call(self, memory, quest_rep, training):
+        # memory (the memory of the previous reasoning step): [batch, d]
+        # quest_rep (self-attention on the BERT-encoded question): [batch, d]
+
+        hidden1 = self.dense1(tf.concat([memory, quest_rep], 1))
+        hidden1 = tf.nn.relu(hidden1)
+        hidden2 = self.dense2(hidden1)
+
+        return hidden2 # [batch, classes]
+
+class MAC_Cell(tf.nn.rnn_cell.RNNCell):
+
+    def __init__(self, knowledge, question, question_rep, d_model, classes, reuse=None): #, num_layers, num_heads, dff, rate):
+        super(MAC_Cell, self).__init__(_reuse=reuse)
+        self.controller = Controller(d_model) #, num_layers, num_heads, dff, rate)
+        self.reader = Reader(d_model) #, num_layers, num_heads, dff, rate)
+        self.write = Writer(d_model)
+        self.all_results = tf.constant(tf.ones(2 * d_model))
+        self.knowledge = knowledge
+        self.question = question
+        self.question_state = tf.keras.layers.Dense(d_model)
+        self.question_rep = question_rep
+
+
+    def call(self, x, h, training):
+        # h (the control and memory of the previous step, respectively): [2d]
+        # x (the temporal encoding of the reasoning step): [d]
+
+        quest_state = self.question_state(x * self.question_rep)
+        prev_control, prev_memory = tf.split(h, 2)
+
+        new_control, control_attention = self.controller(quest_state, prev_control, self.question, training)
+        read, read_attention = self.reader(prev_memory, self.knowledge, new_control, training)
+        new_memory = self.writer(prev_memory, read, new_control, self.all_results, training)
+        # output = self.output(new_memory, self.question, training)
+
+        result = tf.concat([new_control, new_memory])
+        self.all_results = tf.concat([self.allresults, result], 1)
+
+        return result, result
---
+++ container/RACE/MAC_network.py
@@ -0,0 +1,105 @@
+# Copyright 2017-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+#     http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
+# A sample training component that trains a simple scikit-learn decision tree model.
+# This implementation works in File mode and makes no assumptions about the input file names.
+# Input is specified as CSV with a data point in each row and the labels in the first column.
+"""Contains definitions for the preactivation form of Residual Networks.
+
+Residual networks (ResNets) were originally proposed in:
+[1] Kaiming He, Xiangyu Zhang, Shaoqing Ren, Jian Sun
+    Deep Residual Learning for Image Recognition. arXiv:1512.03385
+
+The full preactivation 'v2' ResNet variant implemented in this module was
+introduced by:
+[2] Kaiming He, Xiangyu Zhang, Shaoqing Ren, Jian Sun
+    Identity Mappings in Deep Residual Networks. arXiv: 1603.05027
+
+The key difference of the full preactivation 'v2' variant compared to the
+'v1' variant in [1] is the use of batch normalization before every weight layer
+rather than after.
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+from MACRNN import *
+import tensorflow as tf
+from transformer import Transformer
+
+def get_angles(pos, i, d_model):
+    angle_rates = 1 / np.power(10000, (2 * (i // 2)) / np.float32(d_model))
+    return pos * angle_rates
+
+
+def positional_encoding(position, d_model):
+    angle_rads = get_angles(np.arange(position)[:, np.newaxis],
+                            np.arange(d_model)[np.newaxis, :],
+                            d_model)
+
+    # apply sin to even indices in the array; 2i
+    sines = np.sin(angle_rads[:, 0::2])
+
+    # apply cos to odd indices in the array; 2i+1
+    cosines = np.cos(angle_rads[:, 1::2])
+
+    pos_encoding = np.concatenate([sines, cosines], axis=-1)
+
+    pos_encoding = pos_encoding[np.newaxis, ...]
+
+    return tf.cast(pos_encoding, dtype=tf.float32)
+
+
+def MAC_network_generator(d_model, num_classes, max_steps):
+    """Generator for CIFAR-10 ResNet v2 models.
+
+    Args:
+        d_model: A single integer for the size of the model.
+        num_classes: The number of possible classes for image classification.
+        max_steps: The maximum number of reasoning steps.
+
+    Returns:
+        The model function that takes in `inputs` and `is_training` and
+        returns the output tensor of the MAC Network model.
+
+    Raises:
+        ValueError: If `resnet_size` is invalid.
+    """
+
+    def model(knowledge, question, is_training):
+        """Constructs the MAC Network model given the inputs."""
+
+        step_encoder = positional_encoding(max_steps, d_model)
+
+        decoder1 = tf.keras.layers.dense(768)
+        decoder2 = tf.keras.layers.dense(512)
+
+        decoded_knowledge = tf.nn.tanh(decoder1(knowledge))
+        decoded_knowledge = tf.nn.tanh(decoder2(decoded_knowledge))
+        decoded_quest = tf.nn.tanh(decoder1(question))
+        decoded_quest = tf.nn.tanh(decoder2(decoded_quest))
+
+        context_words = Transformer(2, d_model, 8, d_model)(decoded_quest, decoded_quest, is_training)
+        question_repre = tf.reduce_mean(context_words, 1)
+
+        mac_cell = MAC_Cell(decoded_knowledge, context_words, question_repre, d_model, num_classes)
+        final_output, __ = tf.nn.dynamic_rnn(mac_cell, step_encoder, training=is_training)
+
+        __, memory = tf.split(final_output, 2)
+
+        output = Output(d_model, num_classes)(memory, question_repre)
+
+        return output # Softmax classified [batch, classes]
+
+    return model
